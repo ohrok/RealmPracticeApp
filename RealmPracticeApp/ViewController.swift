9840a164bc4cf4e61a8342cd656775ee8bb6b37a
ohrok <oh.hymh@gmail.com>
2021-08-09 13:12:07 +0900

    Initial Commit

---
+++ RealmPracticeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmPracticeApp
+//
+//  Created by 大井裕貴 on 2021/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
