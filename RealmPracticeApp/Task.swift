9bb13256f86c01aceca8e7f65ff00d13920adf61
ohrok <oh.hymh@gmail.com>
2021-08-09 14:16:45 +0900

    add tableView

---
+++ RealmPracticeApp/Task.swift
@@ -0,0 +1,19 @@
+//
+//  Task.swift
+//  RealmPracticeApp
+//
+//  Created by 大井裕貴 on 2021/08/09.
+//
+
+import RealmSwift
+
+class Task: Object {
+  
+  @Persisted var name: String = ""
+  @Persisted var isChecked: Bool = false
+  
+  convenience init(name: String) {
+    self.init()
+    self.name = name
+  }
+}
---
+++ RealmPracticeApp/TaskListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TaskListViewController.swift
+//  RealmPracticeApp
+//
+//  Created by 大井裕貴 on 2021/08/09.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    
+}
+
+extension TaskListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension TaskListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
+        cell.textLabel?.text = "Task\(indexPath.row + 1)"
+        return cell
+    }
+}
